e821def8f353087c3b2b787a820c198e8ba97c22
jeroenflvr <jeroen@flexworks.eu>
2024-09-15 14:13:03 +0200

    chore: naming

--- src/rest_api_extension.cpp
+++ src/rest_api_extension.cpp
@@ -1,6 +1,6 @@
 #define DUCKDB_EXTENSION_MAIN
 
-#include "rest_api_extension_extension.hpp"
+#include "rest_api_extension.hpp"
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
