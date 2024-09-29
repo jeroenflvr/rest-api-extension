fff0f0185b42413b12ffc5052aa92adb93650c1b
jeroenflvr <jeroen@flexworks.eu>
2024-09-29 14:06:11 +0200

    doc: cleanup

--- src/include/logger.hpp
+++ src/include/logger.hpp
@@ -4,25 +4,20 @@
 #include <string>
 #include <fstream>
 
-// Logger class definition
 class Logger {
 public:
-    // Log levels enumeration
-    enum LogLevel {
+   enum LogLevel {
         DEBUG,
         INFO,
         WARN,
         ERROR
     };
 
-    // Constructor and destructor
     Logger(const std::string& filename);
     ~Logger();
 
-    // Log function with contextual information
     void log(LogLevel level, const std::string& message, const std::string& file, const std::string& function, int line);
 
-    // Macros for easier logging
     #define LOG_DEBUG(message) log(Logger::DEBUG, message, __FILE__, __FUNCTION__, __LINE__)
     #define LOG_INFO(message)  log(Logger::INFO, message, __FILE__, __FUNCTION__, __LINE__)
     #define LOG_WARN(message)  log(Logger::WARN, message, __FILE__, __FUNCTION__, __LINE__)
@@ -31,7 +26,6 @@ public:
 private:
     std::ofstream logFile;
 
-    // Helper functions to get timestamp and log level names
     std::string getCurrentTimestamp();
     std::string getLogLevelName(LogLevel level);
 };
